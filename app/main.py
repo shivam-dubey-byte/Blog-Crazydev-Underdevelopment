97b1748a2e3b9b143f58a2bf62b1c94d9fd151e4
Shivam <dubeyshivamraj@gmail.com>
2021-05-21 00:03:06 +0530

    make it better

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, session, redirect, request
-
+'''
 from flask_hashing import Hashing
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -9,7 +9,7 @@ from email_validator import validate_email, EmailNotValidError
 
 with open('config.json','r') as c:
     params = json.load(c)['params']
-
+'''
 app = Flask(__name__)
 
 def verify_email(em):
