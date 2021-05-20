8ab46b1fbd418099457b3f1d96f2a31fc91b927a
Shivam <dubeyshivamraj@gmail.com>
2021-05-20 17:24:21 +0530

    make it better

--- app/main.py
+++ app/main.py
@@ -5,4 +5,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html',message='Welcome')
+@app.route("/gallery")
+def _gallery_():
+    return render_template('pages/gallery.html')
+@app.route("/full-width")
+def _full_width_():
+    return render_template('pages/full-width.html')
+@app.route("/sidebar-left")
+def _sidebar_left_():
+    return render_template('pages/sidebar-left.html')
+@app.route("/sidebar-right")
+def _sidebar_right_():
+    return render_template('pages/sidebar-right.html')
+@app.route("/basic-grid")
+def _basic_grid_():
+    return render_template('pages/basic-grid.html')
+@app.route("/font-icons")
+def _font_icons_():
+    return render_template('pages/font-icons.html',form_submit='/sign-up')
 
