1312a8ca6063d0d241c1ffb9c8bae4b28c55ebce
Shivam <dubeyshivamraj@gmail.com>
2021-05-20 17:11:26 +0530

    make it better

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-		return "<h1>Welcome to Geeks for Geeks</h1>"
+		return render_template('index.html')
 
